006fa09c93f79182bf43dac74846a1bc1282d16c
vinodh241 <a.vinodh9072@gmail.com>
2026-01-02 12:53:13 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,24 @@
 output "vpc_id" {
+
+
     
-    value = aws_vpc.main.id
+                                                  value = aws_vpc.main.id
 }
 
-output "public_subent_ids" {
+output "public_subnet_ids" {
   
-  value = aws_subnet.public[*].id
+                  value = aws_subnet.public[*].id
 }
 
 
-output "private_subent_ids" {
+output "private_subnet_ids" {
+
   
-  value = aws_subnet.private[*].id
+                               value = aws_subnet.private[*].id
 }
 
-output "database_subent_ids" {
+output "database_subnet_ids" {
+
   
-  value = aws_subnet.database[*].id
+                                                                value = aws_subnet.database[*].id
 }
